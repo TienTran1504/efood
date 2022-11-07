53542c175df47b7aac8939881a03f99c5b251ca6
20127557_long <74775916+longdzvcl123@users.noreply.github.com>
2022-11-07 08:12:08 +0700

    Slider (#3)
    Co-authored-by: Hasaghi <tblonga6nd2020@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {publicRoutes.map((route, index) => {
-            const Page = route.component;
-            let Layout = DefaultLayout;
-            if (route.layout) {
-              Layout = route.layout;
-            } else if (route.layout === null) {
-              Layout = Fragment;
-            }
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
-
-            return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
-          })}
-
-        </Routes>
-        <h1> Hi</h1>
-      </div>
-    </Router>
-  );
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+                <h1> Hi</h1>
+            </div>
+        </Router>
+    );
 }
 export default App;

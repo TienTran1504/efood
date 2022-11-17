b386b80aeae82af34cc2a4e59845e38e7e634440
Hasaghi <tblonga6nd2020@gmail.com>
2022-11-17 17:49:30 +0700

    UpDateSlider

--- Front-end/Customer/src/components/Layout/DefaultLayout/Slider/SliderData.js
+++ Front-end/Customer/src/components/Layout/DefaultLayout/Slider/SliderData.js
@@ -6,7 +6,7 @@ const sliderHomeItems = {
         { image: 'https://i.imgur.com/CUG0Aof.jpeg' },
         { image: 'https://i.imgur.com/mjicxPK.jpeg' },
     ],
-    height: '70vh',
+    height: '87.9vh',
     slogan: {
         heading__2: 'Are You Hungry ?',
         heading__1: "Don't Wait !",

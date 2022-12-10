0260a7a4ae57b48492debb91581f9fbd21a0096d
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2022-12-10 15:11:13 +0700

    Build API get Food By Price, Rating

--- Back-end/EFOOD-API/routes/foods.js
+++ Back-end/EFOOD-API/routes/foods.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllFoods, getFood, updateFood, createFood, deleteFood, ratingFood, getFoodsByType } = require('../controllers/foods')
+const { getAllFoods, getFood, updateFood, createFood, deleteFood, ratingFood, getFoodsByType, getFoodsByPrice } = require('../controllers/foods')
 
 router.route('/').post(createFood).get(getAllFoods)
 router.route('/type').get(getFoodsByType)
+router.route('/price').get(getFoodsByPrice)
 router.route('/:id').get(getFood).delete(deleteFood).patch(updateFood)
 router.route('/rating/:id').patch(ratingFood)
 

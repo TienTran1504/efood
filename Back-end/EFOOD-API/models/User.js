1e75fc99f472a4fd6978f3ff120d59aaaed5c206
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-01-05 15:17:18 +0700

    Test Deploy

--- Back-end/EFOOD-API/models/User.js
+++ Back-end/EFOOD-API/models/User.js
@@ -28,9 +28,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         // required: [true, 'Please provide phone number'],
         match: [/(84|0[3|5|7|8|9])+([0-9]{8})\b/g, "Please provide valid phone number"],
-        unique: true,
         trim: true,
-
     },
     address: {
         type: String,
